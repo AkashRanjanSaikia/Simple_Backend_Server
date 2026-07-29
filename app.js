599cb6afc3b74dd637a7d877eec709eec8d01369
Akash Saikia <arsaikia26@gmail.com>
2026-07-29 19:40:45 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.get("/",(req,res)=>{
+    console.log("Received");
+    res.send("Hii From Server");
+})
+
+app.listen(80 , ()=>{
+    console.log("Server is Running");
+})
