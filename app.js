647d68db6a88cb5fe79df949ee77237510887c15
Akash Saikia <arsaikia26@gmail.com>
2026-09-08 20:50:57 +0530

    removing the health check

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ app.get("/",(req,res)=>{
     res.send("Hello Akash Ranjan Saikia");
 })
 
-app.get("/health",(req,res)=>{
-    res.status(200).send("ok");
-})
+// app.get("/health",(req,res)=>{
+//     res.status(200).send("ok");
+// })
 
 app.listen(3000 , ()=>{
     console.log("Server is Running");
