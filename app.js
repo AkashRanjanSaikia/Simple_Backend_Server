a217c5024a9c4f1913d02479a0250d9cdc007728
Akash Saikia <arsaikia26@gmail.com>
2026-09-10 21:54:47 +0530

    trigger 5

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(cors());
 
 app.get("/",(req,res)=>{
     console.log("Received");
-    res.send("This is a test msg without health check point trigger 4");
+    res.send("This is a test msg without health check point trigger 5");
 })
 
 // app.get("/health",(req,res)=>{
